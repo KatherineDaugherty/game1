9650190135b2fc9b916ba8f6523198285f2c208b
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-12-28 15:25:27 -0600

    created an alert for losing

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var character =
+let character =
     document.getElementById("character");
 
-var block = document.getElementById("block");
+let block = document.getElementById("block");
 
 function jump() {
     if (character.classList != "animate") {
@@ -11,3 +11,18 @@ function jump() {
         character.classList.remove("animate");
     }, 500);
 }
+
+let checkDead = setInterval (function() {
+    
+    var characterTop =
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    
+    let  blockLeft =
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    
+    if( blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+            block.style.animation = "none";
+            block.style.display = "none";
+            alert(" you lose ");
+        }
+}, 10);
